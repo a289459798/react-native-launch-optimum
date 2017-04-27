7ba5ba89ad16addb37fc1e454a42cadb190d20ba
zhangzy <zhangzy@zhangzydeMacBook-Pro.local>
2017-04-27 13:53:49 +0800

    第一个版本

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # react-native-launch-optimum
-优化react-native 启动时间
+
+优化react-native android 启动时间
+
+### 安装
+
+```
+npm install react-native-launch-optimum
+react-native link
+```
+
+### 使用
+
+在启动页增加如下代码
+
+```java
+RNCacheViewManager.init(this, component_name, ((ViewGroup) findViewById(android.R.id.content)));
+```
+
+component_name 为 MainActivity设置的component_name
---
+++ android/src/main/java/com/zhangzy/ReactLaunch/RNCacheViewManager.java
@@ -0,0 +1,57 @@
+package com.zhangzy.ReactLaunch;
+
+import android.app.Activity;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.LinearLayout;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactRootView;
+
+/**
+ * Created by zzy on 2017/4/27.
+ * Date : 2017/4/27 11:31
+ */
+public class RNCacheViewManager {
+    private static ReactRootView mRootView = null;
+    private static ReactInstanceManager mManager = null;
+
+    public static ReactRootView getmRootView() {
+
+        ViewParent viewParent = mRootView.getParent();
+        if (viewParent != null) {
+            ViewGroup vp = (ViewGroup) viewParent;
+            vp.removeView(mRootView);
+        }
+        return mRootView;
+    }
+
+    protected static ReactNativeHost getReactNativeHost(Activity activity) {
+        return ((ReactApplication) activity.getApplication()).getReactNativeHost();
+    }
+
+    public static void init(Activity act, String appkey, ViewGroup view) {
+        if (mManager == null) {
+            mManager = getReactNativeHost(act).getReactInstanceManager();
+        }
+        mRootView = new ReactRootView(act);
+        mRootView.startReactApplication(mManager, appkey, null);
+
+        if (mRootView == null || view == null)
+            return;
+        ViewGroup.LayoutParams params = new LinearLayout.LayoutParams(1, 1);
+        view.addView(mRootView, params);
+    }
+
+    public static void onDestroy() {
+        try {
+            ViewParent parent = getmRootView().getParent();
+            if (parent != null)
+                ((android.view.ViewGroup) parent).removeView(getmRootView());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+}
